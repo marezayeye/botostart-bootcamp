a390c2cb421ad9b954697a03383edcbb46f7d039
marezayeye <marezayeye@gmail.com>
2025-07-11 20:41:00 +0330

    week15 : fixed base project bug

--- mohammadreza-pouladvand_week15/src/App.jsx
+++ mohammadreza-pouladvand_week15/src/App.jsx
@@ -1,6 +1,12 @@
-
+import Input from "./Input";
+const handleChange = () => {};
+const hint = () => {};
 function App() {
-   return <div></div>
+  return (
+    <>
+      <Input handleChange={handleChange} hint={hint} />
+    </>
+  );
 }
 
 export default App;
